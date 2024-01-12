04bf11cf5b5f4f2e2486abe3e1faa5ad930b087e
CharlieH7 <hope91824@gmail.com>
2024-01-13 00:07:12 +1100

    fixed route on hotcollection, new items and top sellers with nft images and author directing to appropraite path

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -47,7 +47,7 @@ const HotCollections = () => {
               <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={collection.id}>
                 <div className="nft_coll">
                   <div className="nft_wrap">
-                    <Link to="/item-details">
+                    <Link to={`/item-details/${collection.nftId}`}>
                       <img src={collection.nftImage} className="lazy img-fluid" alt={collection.title} />
                     </Link>
                   </div>
